bb0f239dcb8d193f2a6bc3f7964188722f352fd3
GSisodiyaShweta <sksisodi@ucsc.edu>
2024-11-08 07:39:38 -0800

    pushing new version

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,19 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "40%";
-    document.getElementById("mySidebar").style.display = "block";
+const navRightAlt = document.querySelector(".nav-right-alt");
+const hamButton = document.querySelector(".ham-icon");
+
+function handleResize() {
+  if (window.innerWidth > 1024) {
+    navRightAlt.classList.remove("show");
   }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
-  document.addEventListener("DOMContentLoaded", function(event) { 
-      document.getElementById('last-updated').textContent = document.lastModified;
-    });
+}
+
+hamButton.addEventListener("click", () => {
+  // Toggle the 'show' class to hide or display the menu
+  navRightAlt.classList.toggle("show");
+});
+
+// Add event listener for window resize
+window.addEventListener("resize", handleResize);
+
+// Initial check in case the page is already wide
+handleResize();
